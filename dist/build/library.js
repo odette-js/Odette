fd57ef24bdbefa254a3a7cb9f55d49237d014558
mmclau14 <michael.j.mclaughlin20@gmail.com>
2016-07-28 21:46:43 -0500

    build

--- dist/build/library.js
+++ dist/build/library.js
@@ -7521,7 +7521,7 @@ app.scope(function (app) {
                     if (memo_) {
                         openBlock(selector, memo);
                     }
-                    result = reduce(json, function (memo, block, key) {
+                    result = foldl(json, function (memo, block, key) {
                         var timmed = key.trim();
                         // var media = trimmed[0] === '@';
                         // if (media) {
