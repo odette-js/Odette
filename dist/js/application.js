a8ec589380350b7ef18ed08e09a52684999690f5
mmclau14 <michael.j.mclaughlin20@gmail.com>
2016-09-19 11:01:29 -0500

    removed dist from git

--- dist/js/application.js
+++
@@ -1,6 +0,0 @@
-Odette(this, 'application', '0.0.0', function (innerSharedApp, scopedApp) {
-    // custom setup code for this version
-    // global app is the object that will be shared with all other iframes
-}, function () {
-    //
-}, [window.parent]);
--- dist/js/test/logger.js
+++
@@ -1,6 +0,0 @@
-application.scope().run(window, function (app, _, factories, documentView, scopedFactories, $) {
-    app.BROWSERSTACKING = true;
-    _.console.log = function (singleton) {
-        window.CONSOLE_LOG = singleton;
-    };
-});
