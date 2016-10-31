94c8d641f6edcfee31e14e6a23499c797148a98b
mmclau14 <michael.j.mclaughlin20@gmail.com>
2016-10-31 16:45:34 -0500

    updated dependency manager to have failed state

--- src/lib/directives/DependencyManager.js
+++ src/lib/directives/DependencyManager.js
@@ -5,24 +5,38 @@ var DependencyManager = Collection.extend('DependencyManager', {
         return this;
     },
     check: function () {
-        var deps = this;
+        var rejection, deps = this;
         if (!deps.length()) {
             return this;
         }
         if (!deps.find(function (promise) {
+                if (promise.is('rejected')) {
+                    rejection = BOOLEAN_TRUE;
+                }
                 if (!promise.is('fulfilled')) {
                     return promise;
                 }
             })) {
+            if (rejection) {
+                deps.failed();
+            }
             deps.reset();
-            deps.target.dispatchEvent('dependencies:finished');
+            deps.finished();
+        } else if (rejection) {
+            deps.failed();
         }
+    },
+    failed: function () {
+        this.target.dispatchEvent('dependencies:failed');
+    },
+    finished: function () {
+        this.target.dispatchEvent('dependencies:finished');
     },
     depend: function (promises) {
         var deps = this,
             registry = deps.directive(REGISTRY);
         Collection(promises).each(function (promise) {
-            if (deps.add(promise)) {
+            if (deps.add(Promise(promise))) {
                 promise.then(registry.get('prebound', 'check', function () {
                     return bind(deps.check, deps);
                 }));
