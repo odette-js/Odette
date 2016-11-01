2e6bcac6bd52edecc7be3d35b7e96c86ace037a6
mmclau14 <michael.j.mclaughlin20@gmail.com>
2016-11-01 18:20:20 -0500

    updated doma to expose a has manager method

--- src/lib/DOMA.js
+++ src/lib/DOMA.js
@@ -2523,7 +2523,7 @@ app.scope(function (app) {
                     return item === manager || item === manager.element() ? manager : returnsManager(item, manager);
                 },
                 hasManager: function (element) {
-                    return element && !isWindow(element) && element.isValidDomManager ? element : !!owner.data.get(el)[DOM_MANAGER_STRING];
+                    return element && !isWindow(element) && element.isValidDomManager ? element : !!manager.data.get(el)[DOM_MANAGER_STRING];
                 },
                 expandEvent: function (passedEvent, actualEvent) {
                     var expanders = manager.events.expanders;
