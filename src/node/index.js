8348c851100604a8d352a82fbfb87519bc98c2a3
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-01-29 16:39:12 -0600

    force added library and odette to be used in node export

--- src/node/index.js
+++ src/node/index.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 var path = require('path');
-var Odette = require(path.join(__dirname, '../../dist/build/odette.js'));
+var Odette = require(path.join(__dirname, '../../dist/build/odette.min.js'));
 var app = Odette(global, 'application', '0.0.0');
 var application = app.global;
-var lib = fs.readFileSync(path.join(__dirname, '../../dist/build/library.js')) + '';
+var lib = fs.readFileSync(path.join(__dirname, '../../dist/build/library.min.js')) + '';
 var fn = new Function.constructor('Odette', 'module', 'exports', 'return ' + lib);
 var xprts = {};
 var mod = {
