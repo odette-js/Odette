9b5fe390724f0b8ab433a9a8ec2aa84ee2441548
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-02-01 14:13:57 -0600

    swapped the wrappers to make the module definition happen first

--- src/node/index.js
+++ src/node/index.js
@@ -1,14 +1,9 @@
 var fs = require('fs');
 var path = require('path');
-var Odette = require(path.join(__dirname, '../../dist/build/odette.min.js'));
+var Odette = require(path.join(__dirname, '../../dist/build/odette.js'));
 var app = Odette(global, 'application', '0.0.0');
 var application = app.global;
-var lib = fs.readFileSync(path.join(__dirname, '../../dist/build/library.min.js')) + '';
-var fn = new Function.constructor('Odette', 'module', 'exports', 'return ' + lib);
-var xprts = {};
-var mod = {
-    exports: xprts
-};
-fn(Odette, mod, xprts);
+global.Odette = Odette;
+require(path.join(__dirname, '../../dist/build/library.js'));
 application.definition(app.VERSION, global);
 module.exports = app;
